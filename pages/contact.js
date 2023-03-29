f9177927a5d9e22a70bca98a1efa428d27172224
r-yanagimoto <r-yanagimoto@ei.sanken.osaka-u.ac.jp>
2023-03-29 21:33:26 +0900

    ⚡️ コンタクトページのレイアウトを修正

--- pages/contact.js
+++ pages/contact.js
@@ -1,7 +1,23 @@
 
 const Contact = () => {
     return (
-        <h1>コンタクトページ</h1>
+        <>
+            <div>
+                <div>
+                    <h1>Contact</h1>
+                    <p>お気軽にご連絡ください</p>
+                    <form>
+                        <label htmlFor="name">お名前</label>
+                        <input type="text" name="name" id="name" required />
+                        <label htmlFor="email" name="email" id="email" required />
+                        <input type="text" name="email" id="email" required />
+                        <label htmlFor="textarea">ご用件</label>
+                        <textarea name="message" rows="10" id="textarea" required />
+                        <button type="submit">送信</button>
+                    </form>
+                </div>
+            </div>
+        </>
     )
 }
 
