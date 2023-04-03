c2d66a279f153f0ccb59e3b3b90a09999d0d71fa
r-yanagimoto <r-yanagimoto@ei.sanken.osaka-u.ac.jp>
2023-04-03 22:11:13 +0900

    ⚡️ sns連携

--- components/footer.js
+++ components/footer.js
@@ -5,9 +5,9 @@ const Footer = () => {
     return (
         <footer>
             <div className='insideContainer'>
-                <a href="https://www.google.com"><img src="/images/github.svg" alt="logo"/></a>
-                <a href="https://www.google.com"><img src="/images/twitter.svg" alt="logo"/></a>
-                <a href="https://www.google.com"><img src="/images/facebook.svg" alt="logo"/></a>
+                <a href="https://github.com/ryo3568"><img src="/images/github.jpg" alt="logo"/></a>
+                <a href="https://twitter.com/ryoyanagi314"><img src="/images/twitter.jpg" alt="logo"/></a>
+                <a href="https://www.facebook.com/profile.php?id=100089916095333"><img src="/images/facebook.jpg" alt="logo"/></a>
                 <hr/>
                 <Link href="/blog">Blog</Link>
                 <Link href="/contact">Contact</Link>
